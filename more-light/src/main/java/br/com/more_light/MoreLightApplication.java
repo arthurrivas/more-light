73d3446bd59b6b682930e39881e81dbeaaf29345
arthur rivas <arthurrivas1@gmail.com>
2025-02-21 07:38:42 -0300

    created project

---
+++ more-light/src/main/java/br/com/more_light/MoreLightApplication.java
@@ -0,0 +1,13 @@
+package br.com.more_light;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreLightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoreLightApplication.class, args);
+	}
+
+}
---
+++ more-light/src/test/java/br/com/more_light/MoreLightApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.more_light;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreLightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
